0f6ee720d744eb8bad89a2d80752f87c75190c11
vison <18526380267@163.com>
2024-05-08 16:36:34 +0800

    feat: #0001 init

---
+++ account/account-service-impl/src/main/java/com/relly/account/service/AccountApplication.java
@@ -0,0 +1,9 @@
+package com.relly.account.service;
+
+/**
+ * @author vison
+ * @Description: 账户模块启动类
+ * @date 2024/5/8 14:20
+ */
+public class AccountApplication {
+}
---
+++ trade/trade-service-impl/src/main/java/com/relly/account/service/TradeApplication.java
@@ -0,0 +1,9 @@
+package com.relly.account.service;
+
+/**
+ * @author vison
+ * @Description: 交易模块启动类
+ * @date 2024/5/8 14:20
+ */
+public class TradeApplication {
+}
